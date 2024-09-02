fc3b219886bcb5be791dc818e9c9b5077ff8fd48
Sophie Muchiri <muthonisophie12@gmail.com>
2024-09-02 22:43:43 +0300

    Inheritance 1

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,27 @@
 from user import User
 
 class Student(User):
+
+    knowledge = []
+
+    def __init__(self,first_name,last_name):
+        super().__init__(first_name,last_name)
+        self.knowledge = Student.knowledge
     
-    def learn(self):
-        pass
+    def learn(self,otherknowledge):
+
+        if isinstance(otherknowledge,str):
+            self.knowledge.append(otherknowledge)
+
+        else:
+            print("error")
+    
+        
+
+
+
+
+
+
+student1 = Student('sophie','muchiri')
+student1.learn(['read'])
